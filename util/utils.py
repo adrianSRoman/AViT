bb3e9bdc398128fdf8e98cab756a5dcd600ca221
asroman <asroman@ucdavis.edu>
2025-03-01 11:02:49 -0800

    include utils to compute seld metrics

--- util/utils.py
+++ util/utils.py
@@ -78,6 +78,80 @@ def print_tensor_info(tensor, flag="Tensor"):
 
 ##################### DCASE Util functions #####################
 
+# TODO: move this to a separate file (utils_seld.py)
+
+def distance_between_cartesian_coordinates(x1, y1, z1, x2, y2, z2):
+    """
+    Angular distance between two cartesian coordinates
+    MORE: https://en.wikipedia.org/wiki/Great-circle_distance
+    Check 'From chord length' section
+
+    :return: angular distance in degrees
+    """
+    # Normalize the Cartesian vectors
+    N1 = np.sqrt(x1**2 + y1**2 + z1**2 + 1e-10)
+    N2 = np.sqrt(x2**2 + y2**2 + z2**2 + 1e-10)
+    x1, y1, z1, x2, y2, z2 = x1/N1, y1/N1, z1/N1, x2/N2, y2/N2, z2/N2
+
+    #Compute the distance
+    dist = x1*x2 + y1*y2 + z1*z2
+    dist = np.clip(dist, -1, 1)
+    dist = np.arccos(dist) * 180 / np.pi
+    return dist
+
+def determine_similar_location(sed_pred0, sed_pred1, doa_pred0, doa_pred1, class_cnt, thresh_unify, nb_classes):
+    if (sed_pred0 == 1) and (sed_pred1 == 1):
+        if distance_between_cartesian_coordinates(doa_pred0[class_cnt], doa_pred0[class_cnt+1*nb_classes], doa_pred0[class_cnt+2*nb_classes],
+                                                  doa_pred1[class_cnt], doa_pred1[class_cnt+1*nb_classes], doa_pred1[class_cnt+2*nb_classes]) < thresh_unify:
+            return 1
+        else:
+            return 0
+    else:
+        return 0
+
+def reshape_3Dto2D(A):
+    return A.reshape(A.shape[0] * A.shape[1], A.shape[2])
+
+def write_output_format_file(_output_format_file, _output_format_dict):
+    """
+    Writes DCASE output format csv file, given output format dictionary
+
+    :param _output_format_file:
+    :param _output_format_dict:
+    :return:
+    """
+    _fid = open(_output_format_file, 'w')
+    # _fid.write('{},{},{},{}\n'.format('frame number with 20ms hop (int)', 'class index (int)', 'azimuth angle (int)', 'elevation angle (int)'))
+    for _frame_ind in _output_format_dict.keys():
+        for _value in _output_format_dict[_frame_ind]:
+            # Write Cartesian format output. Since baseline does not estimate track count and distance we use fixed values.
+            _fid.write('{},{},{},{},{},{},{}\n'.format(int(_frame_ind), int(_value[0]), 0, float(_value[1]), float(_value[2]), float(_value[3]), 0))
+    _fid.close()
+
+def get_multi_accdoa_labels(accdoa_in, nb_classes):
+    """
+    Args:
+        accdoa_in:  [batch_size, frames, num_track*num_axis*num_class=3*3*12]
+        nb_classes: scalar
+    Return:
+        sedX:       [batch_size, frames, num_class=12]
+        doaX:       [batch_size, frames, num_axis*num_class=3*12]
+    """
+    x0, y0, z0 = accdoa_in[:, :, :1*nb_classes], accdoa_in[:, :, 1*nb_classes:2*nb_classes], accdoa_in[:, :, 2*nb_classes:3*nb_classes]
+    sed0 = np.sqrt(x0**2 + y0**2 + z0**2) > 0.5
+    doa0 = accdoa_in[:, :, :3*nb_classes]
+
+    x1, y1, z1 = accdoa_in[:, :, 3*nb_classes:4*nb_classes], accdoa_in[:, :, 4*nb_classes:5*nb_classes], accdoa_in[:, :, 5*nb_classes:6*nb_classes]
+    sed1 = np.sqrt(x1**2 + y1**2 + z1**2) > 0.5
+    doa1 = accdoa_in[:, :, 3*nb_classes: 6*nb_classes]
+
+    x2, y2, z2 = accdoa_in[:, :, 6*nb_classes:7*nb_classes], accdoa_in[:, :, 7*nb_classes:8*nb_classes], accdoa_in[:, :, 8*nb_classes:]
+    sed2 = np.sqrt(x2**2 + y2**2 + z2**2) > 0.5
+    doa2 = accdoa_in[:, :, 6*nb_classes:]
+
+    return sed0, doa0, sed1, doa1, sed2, doa2
+
+
 def load_output_format_file(_output_format_file):
     """
     Loads DCASE output format csv file and returns it in dictionary format
