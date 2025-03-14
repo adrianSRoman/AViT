ee92e1db61e42f0755e002a21f00b8919760a61d
asroman <asroman@ucdavis.edu>
2025-03-13 22:39:41 -0700

    update model by removing feature extraction operations from it

--- model/spatial_ast.py
+++ model/spatial_ast.py
@@ -3,11 +3,8 @@ from functools import partial
 import torch
 import torch.nn as nn
 
-import torchaudio
-
 from timm.models.layers import to_2tuple, trunc_normal_
 
-from utils.stft import STFT, LogmelFilterBank
 from utils.vision_transformer import VisionTransformer as _VisionTransformer
 
 def conv3x3(in_channels, out_channels, stride=1):
@@ -71,33 +68,8 @@ class SpatialAST(_VisionTransformer):
         num_patches = self.patch_embed.num_patches
         self.pos_embed = nn.Parameter(torch.zeros(1, num_patches + 1, emb_dim), requires_grad=False)  # fixed sin-cos embedding
 
-        self.spectrogram_extractor = STFT(
-            n_fft=1024, hop_length=320, win_length=1024, window='hann', 
-            center=True, pad_mode='reflect', freeze_parameters=True
-        )
-        
-        import librosa
-        self.melW = librosa.filters.mel(
-            sr=32000, n_fft=1024, n_mels=128, fmin=50, fmax=14000
-        )
-        self.logmel_extractor = LogmelFilterBank(
-            sr=32000, n_fft=1024, n_mels=128, fmin=50, 
-            fmax=14000, ref=1.0, amin=1e-10, top_db=None, freeze_parameters=True
-        )
-        
-        self.conv_downsample = nn.Sequential(
-            conv3x3(4, 1), 
-            nn.BatchNorm2d(1),
-            nn.GELU(),
-        )
-
-        self.timem = torchaudio.transforms.TimeMasking(192)
-        self.freqm = torchaudio.transforms.FrequencyMasking(48)
-
         self.bn = nn.BatchNorm2d(2, affine=False)
         del self.norm  # remove the original norm
-
-        self.target_frame = 1024
 
         self.dis_norm = kwargs['norm_layer'](emb_dim)
         self.doa_norm = kwargs['norm_layer'](emb_dim)
@@ -124,23 +96,18 @@ class SpatialAST(_VisionTransformer):
         ids_shuffle = torch.argsort(noise, dim=1)  # ascend: small is keep, large is remove
         ids_keep = ids_shuffle[:, :len_keep_T]
         index = ids_keep.unsqueeze(-1).unsqueeze(-1).repeat(1, 1, F, D)
-        #x_masked = torch.gather(x, dim=1, index=index)
-        #x_masked = x_masked.reshape(N,len_keep_T*F,D)
         x = torch.gather(x, dim=1, index=index) # N, len_keep_T(T'), F, D
 
         # mask F
-        #x = x.reshape(N, T, F, D)
         x = x.permute(0, 2, 1, 3) # N T' F D => N F T' D
         len_keep_F = int(F * (1 - mask_f_prob))
         noise = torch.rand(N, F, device=x.device)  # noise in [0, 1]
         # sort noise for each sample
         ids_shuffle = torch.argsort(noise, dim=1)  # ascend: small is keep, large is remove
         ids_keep = ids_shuffle[:, :len_keep_F]
-        #index = ids_keep.unsqueeze(-1).unsqueeze(-1).repeat(1, 1, T, D)
         index = ids_keep.unsqueeze(-1).unsqueeze(-1).repeat(1, 1, len_keep_T, D)
         x_masked = torch.gather(x, dim=1, index=index)
         x_masked = x_masked.permute(0,2,1,3) # N F' T' D => N T' F' D 
-        #x_masked = x_masked.reshape(N,len_keep*T,D)
         x_masked = x_masked.reshape(N,len_keep_F*len_keep_T,D)
             
         return x_masked, None, None
@@ -163,31 +130,8 @@ class SpatialAST(_VisionTransformer):
 
         return x
 
-    # overwrite original timm
-    def forward(self, waveforms, reverbs, mask_t_prob=0.0, mask_f_prob=0.0):
-        waveforms = torchaudio.functional.fftconvolve(waveforms, reverbs, mode='full')[..., :waveforms.shape[-1]]
-        B, C, T = waveforms.shape
-
-        waveforms = waveforms.reshape(B * C, T)
-        real, imag = self.spectrogram_extractor(waveforms) 
-
-        log_mel = self.logmel_extractor(torch.sqrt(real**2 + imag**2)).reshape(B, C, -1, 128)
-        log_mel = self.bn(log_mel)
-        
-        IPD = torch.atan2(imag[1::2], real[1::2]) - torch.atan2(imag[::2], real[::2])
-        x = torch.cat([log_mel, torch.matmul(torch.cat([torch.cos(IPD), torch.sin(IPD)], dim=1), self.logmel_extractor.melW)], dim=1)
-        # x = log_mel
-
-        if x.shape[2] < self.target_frame:
-            x = nn.functional.interpolate(x, (self.target_frame, x.shape[3]), mode="bicubic", align_corners=True)
-    
-        x = self.conv_downsample(x)
-        if self.training:
-            x = x.transpose(-2, -1) # bsz, 4, 1024, 128 --> bsz, 4, 128, 1024
-            x = self.freqm(x)
-            x = self.timem(x)
-            x = x.transpose(-2, -1)
-
+    # Only keeping main model architecture components in the forward method
+    def forward(self, x, mask_t_prob=0.0, mask_f_prob=0.0):
         x = self.patch_embed(x)
         x = self.forward_features_mask(x, mask_t_prob=mask_t_prob, mask_f_prob=mask_f_prob)
 
